7f9149f2342c5c2b5fe71322b069bdb1fb83eec7
Manfred <manfred@manfred047.com>
2020-01-20 23:47:41 -0600

    compilados

---
+++ public/quasar/js/3.90a2ad79.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[3],{e875:function(n,e,r){"use strict";r.r(e),r.d(e,"esGeneralForm",(function(){return o}));var o={back_btn:"Regresar"}}}]);
