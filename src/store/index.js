4bcb32a0a3ed5f61691b23231400140d26a45553
Manfred <manfred@manfred047.com>
2020-05-21 18:48:42 -0500

    updated store modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import auth from './auth'
-import lang from './lang'
+import auth from './modules/auth'
+import lang from './modules/lang'
 
 Vue.use(Vuex)
 
 /*
  * If not building with SSR mode, you can
- * directly export the Store instantiation
+ * directly export the Store instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Store instance.
  */
 
 export default function (/* { ssrContext } */) {
