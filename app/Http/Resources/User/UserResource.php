ab13d4d498c6538f7354309a476c3b18566f2285
Manfred <manfred@manfred047.com>
2020-05-21 20:30:05 -0500

    Update UserResource.php

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -15,9 +15,9 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'email' => $this->email,
-            'username' => $this->username
+            'id'        => $this->id,
+            'email'     => $this->email,
+            'username'  => $this->username
         ];
     }
 }
